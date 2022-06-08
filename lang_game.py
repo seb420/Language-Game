f91a2e2bc49665238f40e8714c8c43f17d61baa5
PRATTSE <sebastian.pratt@student.onslow.school.nz>
2022-06-09 11:57:54 +1200

    Menu option designs

--- lang_game.py
+++ lang_game.py
@@ -1,1 +1,37 @@
 
+"""
+menus
+"""
+
+
+"""
+--- Norwegian Game ---
+1. Instructions
+2. English to Norwegian
+3. Norwegian to English
+4. Study
+5. Teacher Menu
+6. Quit
+"""
+
+"""
+----- Language Game -----
+A............Instructions
+B....English to Norwegian
+C....Norwegian to English
+D...................Study
+E............Teacher Menu
+F....................Quit
+"""
+"""
+<<< Norwegian Game >>>
+I. Instructions
+E. English to Norwegian
+N. Norwegian to English
+S. Study
+T. Teacher Menu
+Q. Quit
+"""
+
+
+
