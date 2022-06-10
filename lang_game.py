89f57a10161bdd2f8d00453af33ba031e9ee6e37
PRATTSE <sebastian.pratt@student.onslow.school.nz>
2022-06-10 13:20:41 +1200

    Starting to program menu

--- lang_game.py
+++ lang_game.py
@@ -1,37 +1,51 @@
+##
+# lang_game.py
+# Sebastian Pratt
+# 10/06/2022 -> PRESENT ___________________________
 
-"""
-menus
-"""
+
+# import any necessary functions
+
+# define the constants
+
+# define the variables
+
+def main():
+    """
+    This is to run the main program and manage all the functions within it.
+    """
+
+    instructions()
 
 
-"""
---- Norwegian Game ---
-1. Instructions
-2. English to Norwegian
-3. Norwegian to English
-4. Study
-5. Teacher Menu
-6. Quit
-"""
-
-"""
------ Language Game -----
-A............Instructions
-B....English to Norwegian
-C....Norwegian to English
-D...................Study
-E............Teacher Menu
-F....................Quit
-"""
-"""
-<<< Norwegian Game >>>
-I. Instructions
-E. English to Norwegian
-N. Norwegian to English
-S. Study
-T. Teacher Menu
-Q. Quit
-"""
+def menu():
+    """
+    yes
+    """
 
 
 
+def instructions():
+    """
+    This function will print out the instructions and return to the menu
+    once it is done.
+    """
+    input("Hei og velkommen. This is the Norwegian language game. It is a \n"
+          "great fun and exciting way to practice your Norwegian skills. \n"
+          "To play simply translate the words on screen, if you get a \n"
+          "question right you have the option to double your points if \n"
+          "you get the next question right or you can play it safe and \n"
+          "bank the points. You only have three wrong guesses before the \n"
+          "game is over though so be careful.")
+
+
+main()
+"""
+----- Norwegian Game -----
+1.............Instructions
+2.....English to Norwegian
+3.....Norwegian to English
+4....................Study
+5.............Teacher Menu
+6.....................Quit
+"""
